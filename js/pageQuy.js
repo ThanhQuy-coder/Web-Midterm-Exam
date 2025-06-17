ce1f8d36821ca6bde59b78a78f8b0b292a35e8b5
Thanh Quy <quyht2908@gmail.com>
2025-06-17 23:18:49 +0700

    Complete page about me and language

--- js/pageQuy.js
+++ js/pageQuy.js
@@ -6,3 +6,29 @@ var typed = new Typed(".text", {
   backDelay: 1000,
   loop: true,
 });
+
+// The shooting star effect only starts running when scrolling
+// to the banner section, and turns off when exiting that area.
+document.addEventListener("DOMContentLoaded", () => {
+  const bannerSection = document.querySelector("#home");
+  const stars = document.querySelectorAll(".shooting-star");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          stars.forEach((star) => star.classList.add("active"));
+        } else {
+          stars.forEach((star) => star.classList.remove("active"));
+        }
+      });
+    },
+    {
+      threshold: 0.3,
+    }
+  );
+
+  if (bannerSection) {
+    observer.observe(bannerSection);
+  }
+});
