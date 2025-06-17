90f5d5da0940c055e49ae173eb97cd97d560e0bf
Thanh Quy <quyht2908@gmail.com>
2025-06-17 20:35:20 +0700

    Complete page banner and add continue page about me

---
+++ js/pageQuy.js
@@ -0,0 +1,8 @@
+// Make the text move
+var typed = new Typed(".text", {
+  strings: ["Backend Developer", "Student", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
